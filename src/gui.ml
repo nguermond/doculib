aa893ac89cff13f99ac71880163ccc0efa45a764
nguermond <nguermond@gmail.com>
2022-03-22 14:36:58 -0500

    move icon location to /usr/local/lib

--- src/gui.ml
+++ src/gui.ml
@@ -2,7 +2,7 @@
 open StdLabels
 open Gobject.Data
 
-let icon_path = Db.configdir^"/icons/Gnome-colors-applications-office.svg"
+let icon_path = "/usr/local/lib/doculib/icons/Gnome-colors-applications-office.svg"
 
 
   
