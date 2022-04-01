58281e6b7d5925a6011c1949d335668e05d2a982
nguermond <nguermond@gmail.com>
2022-04-01 15:50:47 -0500

    remove build file

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dune build
-mkdir $HOME/.doculib
-mkdir $HOME/.doculib/icons
-cp icons/Gnome-colors-applications-office.svg $HOME/.doculib/icons/Gnome-colors-applications-office.svg
-mv _build/default/doculib.exe $HOME/.local/bin/doculib
